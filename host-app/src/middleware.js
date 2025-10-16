21c01c7102613cfc76eb6d3d9603060043b5adcb
najeeb678 <najibqazi777@gmail.com>
2025-10-16 17:05:46 +0500

    updates

--- host-app/src/middleware.js
+++ host-app/src/middleware.js
@@ -1,24 +1,34 @@
-import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
-
+import { getToken } from "next-auth/jwt";
 export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
   const { pathname } = req.nextUrl;
 
-  if (
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/favicon.ico") ||
-    pathname.includes("/_next/static") ||
-    pathname.includes("/_next/image") ||
-    pathname.startsWith("/login") ||
-    pathname.startsWith("/api/auth") ||
-    pathname.startsWith("/v3/publicPage")
-  ) {
-    return NextResponse.next();
+  // if (
+  //   pathname.startsWith("/_next") ||
+  //   pathname.startsWith("/favicon.ico") ||
+  //   pathname.includes("/_next/static") ||
+  //   pathname.includes("/_next/image") ||
+  //   pathname.startsWith("/login") ||
+  //   pathname.startsWith("/api/auth") ||
+  //   pathname.startsWith("/v3/publicPage")
+  // ) {
+  //   return NextResponse.next();
+  // }
+  if (!token) {
+    const loginUrl = new URL("/login", req.url);
+    loginUrl.searchParams.set("redirect", pathname);
+    return NextResponse.redirect(loginUrl);
   }
 
   return NextResponse.next();
 }
 
-export const config = { matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"] };
+export const config = {
+  matcher: [
+    // Apply middleware to all routes EXCEPT these:
+    "/((?!api/auth|login|v3/publicPage|_next/static|_next/image|favicon.ico).*)",
+  ],
+};
+// export const config = { matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"] };
