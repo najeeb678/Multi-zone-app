c4539bb354194dafd1369254bc035141c74e7dec
najeeb678 <najibqazi777@gmail.com>
2025-10-21 13:39:08 +0500

    navbar updates

--- shared-components/src/Components/Navbar.js
+++ shared-components/src/Components/Navbar.js
@@ -5,8 +5,10 @@ import styled from "styled-components";
 
 // Styled components
 const NavbarContainer = styled.div`
-  position: sticky;
+  position: fixed;
   top: 0;
+  left: 0;
+  right: 0;
   z-index: 1000;
   display: flex;
   justify-content: space-between;
